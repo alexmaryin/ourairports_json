635520ff6fdbe571252aba76b62cef06c209b02e
alexmaryin <java73@yandex.ru>
2021-12-02 20:11:59 +0400

    Mapping with batch database replaced with individual upsert in transaction by sequence (due to memory and performance)

--- src/main/kotlin/com/herokuapp/ourairports/data/converters/OurAirportsFactory.kt
+++ src/main/kotlin/com/herokuapp/ourairports/data/converters/OurAirportsFactory.kt
@@ -4,72 +4,70 @@ import com.herokuapp.ourairports.data.model.AirportsTable
 import com.herokuapp.ourairports.data.model.FrequenciesTable
 import com.herokuapp.ourairports.data.model.RunwaysTable
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.batchReplace
-import org.jetbrains.exposed.sql.statements.BatchReplaceStatement
+import org.jetbrains.exposed.sql.replace
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.File
 
 class OurAirportsFactory(private val db: Database) : ConverterFactory {
 
-    private fun getLinesAsSequence(filename: String): List<Map<String, String>> {
+    private fun getLinesAsSequence(filename: String, process: (Map<String, String>) -> Unit) {
         val headers = File("files/$filename").useLines {
             it.firstOrNull()?.replace("\"", "")?.split(",")
         } ?: throw RuntimeException("File $filename has not valid headers")
-        return File("files/$filename").readLines()
-            .drop(1)
+        File("files/$filename").useLines { file ->
+            file.drop(1)
             .map { it.replace("\"", "").split(",") }
             .map { headers.zip(it).toMap() }
-    }
-
-    private fun replaceAirports(receiver: BatchReplaceStatement, row: Map<String, String>) {
-        with(receiver) {
-            row[AirportsTable.id.name]?.toIntOrNull().let { airportId -> this[AirportsTable.id] = airportId }
-            this[AirportsTable.icao] = row[AirportsTable.icao.name].toString().take(4)
-            this[AirportsTable.type] = row["type"].toString().take(50)
-            this[AirportsTable.name] = row[AirportsTable.name.name].toString().take(255)
-            this[AirportsTable.latitude] = row[AirportsTable.latitude.name]?.toFloatOrNull() ?: 0f
-            this[AirportsTable.longitude] = row[AirportsTable.longitude.name]?.toFloatOrNull() ?: 0f
-            this[AirportsTable.elevation] = row[AirportsTable.elevation.name]?.toIntOrNull() ?: 0
-            this[AirportsTable.webSite] = row[AirportsTable.webSite.name]
-            this[AirportsTable.wiki] = row[AirportsTable.wiki.name]
+            .forEach(process)
         }
     }
 
-    private fun replaceRunways(receiver: BatchReplaceStatement, row: Map<String, String>) {
-        with(receiver) {
-            row[RunwaysTable.id.name]?.toIntOrNull()?.let { runwayId -> this[RunwaysTable.id] = runwayId }
-            this[RunwaysTable.airportId] = row[RunwaysTable.airportId.name]?.toIntOrNull() ?: 0
-            this[RunwaysTable.lengthFeet] = row[RunwaysTable.lengthFeet.name]?.toIntOrNull() ?: 0
-            this[RunwaysTable.widthFeet] = row[RunwaysTable.widthFeet.name]?.toIntOrNull() ?: 0
-            this[RunwaysTable.surface] = row[RunwaysTable.surface.name].toString().take(10)
-            this[RunwaysTable.closed] = row[RunwaysTable.closed.name].toBoolean()
-            this[RunwaysTable.lowNumber] = row[RunwaysTable.lowNumber.name].toString().take(10)
-            this[RunwaysTable.lowHeading] = row[RunwaysTable.lowHeading.name]?.toIntOrNull() ?: 0
-            this[RunwaysTable.lowElevationFeet] = row[RunwaysTable.lowElevationFeet.name]?.toIntOrNull() ?: 0
-            this[RunwaysTable.highNumber] = row[RunwaysTable.highNumber.name].toString().take(10)
-            this[RunwaysTable.highHeading] = row[RunwaysTable.highHeading.name]?.toIntOrNull() ?: 0
-            this[RunwaysTable.highElevationFeet] = row[RunwaysTable.highElevationFeet.name]?.toIntOrNull() ?: 0
-        }
+    private fun replaceAirport(row: Map<String, String>) = AirportsTable.replace {
+        row[id.name]?.toIntOrNull().let { airportId -> it[id] = airportId }
+        it[icao] = row[icao.name].toString().take(4)
+        it[type] = row["type"].toString().take(50)
+        it[name] = row[name.name].toString().take(255)
+        it[latitude] = row[latitude.name]?.toFloatOrNull() ?: 0f
+        it[longitude] = row[longitude.name]?.toFloatOrNull() ?: 0f
+        it[elevation] = row[elevation.name]?.toIntOrNull() ?: 0
+        it[webSite] = row[webSite.name]
+        it[wiki] = row[wiki.name]
     }
 
-    private fun replaceFrequencies(receiver: BatchReplaceStatement, row: Map<String, String>) {
-        with(receiver) {
-            row[FrequenciesTable.id.name]?.toIntOrNull()?.let { freqId -> this[FrequenciesTable.id] = freqId }
-            this[FrequenciesTable.airportId] = row[FrequenciesTable.airportId.name]?.toIntOrNull() ?: 0
-            this[FrequenciesTable.type] = row["type"].toString().take(50)
-            this[FrequenciesTable.description] = row[FrequenciesTable.description.name]?.take(100)
-            this[FrequenciesTable.valueMhz] = row[FrequenciesTable.valueMhz.name]?.toFloatOrNull() ?: 0f
-        }
+
+    private fun replaceRunway(row: Map<String, String>) = RunwaysTable.replace {
+        row[id.name]?.toIntOrNull()?.let { runwayId -> it[id] = runwayId }
+        it[airportId] = row[airportId.name]?.toIntOrNull() ?: 0
+        it[lengthFeet] = row[lengthFeet.name]?.toIntOrNull() ?: 0
+        it[widthFeet] = row[widthFeet.name]?.toIntOrNull() ?: 0
+        it[surface] = row[surface.name].toString().take(10)
+        it[closed] = row[closed.name].toBoolean()
+        it[lowNumber] = row[lowNumber.name].toString().take(10)
+        it[lowHeading] = row[lowHeading.name]?.toIntOrNull() ?: 0
+        it[lowElevationFeet] = row[lowElevationFeet.name]?.toIntOrNull() ?: 0
+        it[highNumber] = row[highNumber.name].toString().take(10)
+        it[highHeading] = row[highHeading.name]?.toIntOrNull() ?: 0
+        it[highElevationFeet] = row[highElevationFeet.name]?.toIntOrNull() ?: 0
+    }
+
+    private fun replaceFrequency(row: Map<String, String>) = FrequenciesTable.replace {
+        row[id.name]?.toIntOrNull()?.let { freqId -> it[id] = freqId }
+        it[airportId] = row[airportId.name]?.toIntOrNull() ?: 0
+        it[type] = row["type"].toString().take(50)
+        it[description] = row[description.name]?.take(100)
+        it[valueMhz] = row[valueMhz.name]?.toFloatOrNull() ?: 0f
+
     }
 
     override fun convert(filename: String): Boolean {
-        val batch = getLinesAsSequence(filename)
         transaction(db) {
-            when (filename) {
-                "airports.csv" -> AirportsTable.batchReplace(batch) { row -> replaceAirports(this, row) }
-                "runways.csv" -> RunwaysTable.batchReplace(batch) { row -> replaceRunways(this, row) }
-                "airport-frequencies.csv" -> FrequenciesTable.batchReplace(batch) { row -> replaceFrequencies(this, row) }
-                else -> throw RuntimeException("Mapper for $filename is not implemented!")
+            getLinesAsSequence(filename) { row ->
+                when (filename) {
+                    "airports.csv" -> replaceAirport(row)
+                    "runways.csv" -> replaceRunway(row)
+                    "airport-frequencies.csv" -> replaceFrequency(row)
+                    else -> throw RuntimeException("Mapper for $filename is not implemented!")
+                }
             }
         }
         return true
